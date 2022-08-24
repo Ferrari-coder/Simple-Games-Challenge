6e8eab8975e45654ae78a86053643ac18ccaeafe
Amudipe Emmanuel <abiemma84@gmail.com>
2022-08-24 08:32:23 +0100

    changing players

--- pig games v2/script.js
+++ pig games v2/script.js
@@ -6,15 +6,39 @@ const currentScore2 = document.querySelector("#player2-current");
 const rollDice = document.querySelector(".btn-roll");
 const imageBox = document.querySelector(".img-box");
 const image = document.querySelector(".img-box img");
+let activePlayer = 1;
 
-let score = 0
-rollDice.addEventListener("click",function () {
-    let randomNumber = Math.trunc(Math.random()* 6 + 1)
-    // console.log(randomNumber)
-    image.src=`./image/dice-${randomNumber}.png` 
-    imageBox.classList.remove("hidden")
-    score += randomNumber
-    console.log(score)
-    currentScore1 = score
-})
+//default values
+imageBox.classList.add("hidden");
+currentScore1.textContent = 0;
+currentScore2.textContent = 0;
+const totalScore = [0, 0];
 
+let score = 0;
+rollDice.addEventListener("click", function () {
+  let randomNumber = Math.trunc(Math.random() * 6 + 1);
+  // console.log(randomNumber)
+  image.src = `./image/dice-${randomNumber}.png`;
+  imageBox.classList.remove("hidden");
+  if (randomNumber !== 1) {
+    score += randomNumber;
+    console.log(score);
+    document.querySelector(`#player${activePlayer}-current`).textContent =
+      score;
+  } else {
+    //if dice is 1 switch player
+    // document.querySelector(`.player${activePlayer} .zero`).textContent=score
+
+    score = 0;
+    document.querySelector(`#player${activePlayer}-current`).textContent =
+      score;
+    document
+      .querySelector(`.player${activePlayer}`)
+      .classList.toggle("player-active");
+
+    activePlayer = activePlayer === 1 ? 2 : 1;
+    document
+      .querySelector(`.player${activePlayer}`)
+      .classList.toggle("player-active");
+  }
+});
